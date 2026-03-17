e507c3f0a1317fff2202edd1b17e66665b3d3304
Попова Елена Юрьевна <e.y.popova@tbank.ru>
2026-03-17 18:20:52 +0300

    создан репозиторий

---
+++ src/shared/header/Header.spec.tsx
@@ -0,0 +1,15 @@
+/**
+ * @jest-environment @tramvai/test-unit-jest/lib/jsdom-environment
+ */
+import { testComponent } from '@tramvai/test-react';
+import { Header } from './Header';
+
+describe('Header', () => {
+  it('should render header', () => {
+    const { render } = testComponent(<Header />);
+
+    expect(render.container.innerHTML).toMatchInlineSnapshot(
+      `"<header class="Header"><h1>Tramvai <span role="img" aria-label="Salute">🥳</span></h1></header>"`
+    );
+  });
+});
